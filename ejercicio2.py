aabfb79bf2b8b7bffd94f9d374f0ebb07cba8d06
Darwin <114919297+hikaruwheels@users.noreply.github.com>
2023-05-29 13:11:50 +0200

    Add files via upload

---
+++ ejercicio2.py
@@ -0,0 +1,11 @@
+x = 3
+y = 3.3
+
+print("El tipo de x es ", type(x))
+print("El tipo de y es ", type(y))
+
+x = "3"
+y = "3.3"
+
+print("El tipo de x después es ", type(x))
+print("el tipo de y después es ", type(y))
